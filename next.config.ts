df0226cebe28c5e1e6e4d1ca45f4f32997c90838
Ryoda <ryoda41@gmail.com>
2025-01-02 17:29:13 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,3 +2,11 @@ module.exports = {
   basePath: '/ryoda-pages-v2',  // Укажите имя вашего репозитория
   trailingSlash: true,
 };
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
