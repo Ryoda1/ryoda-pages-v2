4ac02054a145abc9fe3159928b7be5ab54231fc8
AsaNekoo <nekoa931@gmail.com>
2025-01-03 16:20:35 +0100

    я умньі

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,31 @@
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 import type { NextConfig } from "next";
-import { Compiler } from 'webpack';
+// import { Compiler } from 'webpack';
 
 const nextConfig: NextConfig = {
     /* config options here */
     output: "export",
-    webpack: (config) => {
-        config.plugins?.push({
-            apply: (compiler: Compiler) => {
-                compiler.hooks.afterEmit.tap('CopyCNAMEPlugin', () => {
-                    const source = path.join(process.cwd(), 'public', 'CNAME');
-                    const destination = path.join(process.cwd(), 'out', 'CNAME');
-
-                    console.log(`Checking if CNAME exists at: ${source}`);
-                    if (fs.existsSync(source)) {
-                        console.log('CNAME file found, copying to output directory...');
-                        fs.copyFileSync(source, destination);
-                    } else {
-                        console.error('CNAME file not found!');
-                    }
-                });
-            },
-        });
-
-        return config;
-    },
+    // webpack: (config) => {
+    //     config.plugins?.push({
+    //         apply: (compiler: Compiler) => {
+    //             compiler.hooks.afterEmit.tap('CopyCNAMEPlugin', () => {
+    //                 const source = path.join(process.cwd(), 'public', 'CNAME');
+    //                 const destination = path.join(process.cwd(), 'out', 'CNAME');
+    //
+    //                 console.log(`Checking if CNAME exists at: ${source}`);
+    //                 if (fs.existsSync(source)) {
+    //                     console.log('CNAME file found, copying to output directory...');
+    //                     fs.copyFileSync(source, destination);
+    //                 } else {
+    //                     console.error('CNAME file not found!');
+    //                 }
+    //             });
+    //         },
+    //     });
+    //
+    //     return config;
+    // },
 };
 
 export default nextConfig;
