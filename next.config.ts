ba210bc375d8099101ecfdba6fc61cb9c16f09ea
AsaNekoo <nekoa931@gmail.com>
2025-01-03 15:46:09 +0100

    next config change

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,23 @@
 import fs from 'fs';
 import path from 'path';
 import type { NextConfig } from "next";
-import { Compiler } from 'webpack';
+// import { Compiler } from 'webpack';
 
 const nextConfig: NextConfig = {
     /* config options here */
     output: "export",
-    webpack: (config) => {
-        config.plugins?.push({
-            apply: (compiler: Compiler) => {
-                compiler.hooks.afterEmit.tap('CopyCNAMEPlugin', () => {
-                    const source = path.join(process.cwd(), 'public', 'CNAME');
-                    const destination = path.join(process.cwd(), 'out', 'CNAME');
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            const cnamePath = path.join(__dirname, 'public', 'CNAME');
+            const outPath = path.join(__dirname, 'out', 'CNAME');
 
-                    if (fs.existsSync(source)) {
-                        fs.copyFileSync(source, destination);
-                        console.log('CNAME file copied to the out directory.');
-                    }
-                });
-            },
-        });
-
+            if (fs.existsSync(cnamePath)) {
+                fs.copyFileSync(cnamePath, outPath);
+                console.log(`Copied CNAME file from ${cnamePath} to ${outPath}`);
+            } else {
+                console.warn(`CNAME file does not exist at ${cnamePath}`);
+            }
+        }
         return config;
     },
 };
