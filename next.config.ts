1a9e96b4c328ffcd5a9f77e863cb9a2b3cdd7c16
Ryoda <ryoda41@gmail.com>
2025-01-03 22:17:31 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,28 +4,12 @@ import type { NextConfig } from "next";
 // import { Compiler } from 'webpack';
 
 const nextConfig: NextConfig = {
-    /* config options here */
     output: "export",
-    // webpack: (config) => {
-    //     config.plugins?.push({
-    //         apply: (compiler: Compiler) => {
-    //             compiler.hooks.afterEmit.tap('CopyCNAMEPlugin', () => {
-    //                 const source = path.join(process.cwd(), 'public', 'CNAME');
-    //                 const destination = path.join(process.cwd(), 'out', 'CNAME');
-    //
-    //                 console.log(`Checking if CNAME exists at: ${source}`);
-    //                 if (fs.existsSync(source)) {
-    //                     console.log('CNAME file found, copying to output directory...');
-    //                     fs.copyFileSync(source, destination);
-    //                 } else {
-    //                     console.error('CNAME file not found!');
-    //                 }
-    //             });
-    //         },
-    //     });
-    //
-    //     return config;
-    // },
+};
+module.exports = {
+  images: {
+    domains: ['github.com'],
+  },
 };
 
 export default nextConfig;
